fd16af893930ea6c28440023f55edf3990e95854
arikariyanto57-glitch <arikariyanto57@gmail.com>
2025-09-10 13:56:57 +0700

    menambahkan elif untuk pengondisian

--- if-else.py
+++ if-else.py
@@ -3,5 +3,7 @@ umur = int(input("masukkan usia kamu :"))
 
 if umur < 12:
     print ("kamu masih anak anak ")
+elif umur < 21:
+    print ("kamu masih remaja")
 else :
     print("kamu sudah remaja")
