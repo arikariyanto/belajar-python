8fde85d2406e2580a27dfc2b8e66cb2759408901
arikariyanto57-glitch <arikariyanto57@gmail.com>
2025-09-10 22:25:14 +0700

    Add files via upload

---
+++ suhu.py
@@ -0,0 +1,12 @@
+print("PROGRAM MENGHITUNG SUHU")
+
+celcius = float(input("masukkan suhu dalam celcius :"))
+
+if celcius < 0:
+    print ("suhu sekarang adalah beku")
+elif celcius <30:
+    print ("suhu sekarang adalah normal")
+else :
+    print ("suhu sekarang adalah panas")
+
+    
