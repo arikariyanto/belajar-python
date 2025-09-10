5acacb9ff273d1e1fef004a1c505c793a1106d35
arikariyanto57-glitch <arikariyanto57@gmail.com>
2025-09-10 22:34:39 +0700

    Update suhu.py

--- suhu.py
+++ suhu.py
@@ -9,4 +9,11 @@ elif celcius <30:
 else :
     print ("suhu sekarang adalah panas")
 
+fahrenheit = (celcius * 9/5) + 32
+kelvin = celcius + 273.15
+reamur = celcius * 4/5
+
+print ("suhu dalam Fahrenheit:", fahrenheit, "°F")
+print ("suhu dalam Kelvin:", kelvin, "°k")
+print ("suhu dalam Reamur:", reamur, "°Re")
     
