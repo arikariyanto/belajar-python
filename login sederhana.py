1fe9385382ce33f0bd46c3997fb1469a721939e8
arikariyanto57-glitch <arikariyanto57@gmail.com>
2025-09-10 20:33:37 +0700

    Add files via upload

---
+++ login sederhana.py
@@ -0,0 +1,8 @@
+print ("--PROGRAM LOGIN SEDERHANA--")
+admin = input("Masukkan username : ")
+pw = input("Masukkan password : ")
+
+if admin == "Arik" and pw == "1234":
+    print("Kamu berhasil login!") 
+else:
+    print("Login gagal")
