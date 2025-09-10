3a37b85430dad7b6793565ac66eab90119570f43
arikariyanto57-glitch <arikariyanto57@gmail.com>
2025-09-10 22:16:35 +0700

    Update login sederhana.py

--- login sederhana.py
+++ login sederhana.py
@@ -4,5 +4,10 @@ pw = input("Masukkan password : ")
 
 if admin == "Arik" and pw == "1234":
     print("Kamu berhasil login!") 
+elif admin != "Arik" and pw == "1234":
+    print ("username kamu salah")
+elif admin == "Arik" and pw != "1234":
+    print ("password kamu salah")
 else:
     print("Login gagal")
+
