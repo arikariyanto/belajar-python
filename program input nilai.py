78238798efcb989e060285d3b25fb95a4e8692c0
arikariyanto57-glitch <arikariyanto57@gmail.com>
2025-09-10 19:57:35 +0700

    Create program input nilai.py

---
+++ program input nilai.py
@@ -0,0 +1,10 @@
+nilai = int (input("masukkan nilai ujian kamu (0-100) : "))
+
+if nilai >= 90:
+    print ("nilai kamu A")
+elif nilai >= 75:
+    print ("nilai kamu B")
+elif nilai >=50:
+    print ("nilai kamu C")
+else :
+    print ("nilai kamu D")
