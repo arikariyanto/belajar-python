0a8a6d7c47038d23b7510e01fd9a5b08d29e4988
arikariyanto57-glitch <arikariyanto57@gmail.com>
2025-09-11 12:32:19 +0700

    Update menghitung jumlah bilangan

--- looping bilangan.py
+++ looping bilangan.py
@@ -1,5 +1,11 @@
 n = int (input("masukkan angka :"))
 
+count = 0
+print ("\nbilangan kelipatan 3 dari 1 sampai" ,n)
+
 for i in (range (1, n)):
     if i % 3 == 0:
      print (i)
+     count += 1
+
+print (f"\nJumlah bilangan kelipatan 3 sampai {n} adalah {count}")
