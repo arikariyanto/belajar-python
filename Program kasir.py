de21d3e2455220781ac8e8149feb5e30f05fcd2e
arikariyanto57-glitch <arikariyanto57@gmail.com>
2025-09-13 21:37:51 +0700

    menambahkan diskon

--- Program kasir.py
+++ Program kasir.py
@@ -4,10 +4,11 @@ daftar_barang = {
     "apel": 5000,
     "jeruk": 7000,
     "mangga": 8000,
-    "pisang": 4000
+    "pisang": 4000,
+    "manggis": 6500
 }
 
-total = 0
+keranjang = []
 
 while True:
     print("\nDaftar Barang:")
@@ -21,11 +22,26 @@ while True:
 
     if pilih in daftar_barang:
         jumlah = int(input("Masukkan jumlah: "))
-        subtotal = daftar_barang[pilih] * jumlah   # isi bagian kosong
-        total += subtotal
+        subtotal = daftar_barang[pilih] * jumlah   
+        keranjang.append((pilih,jumlah,subtotal))
         print(f"Ditambahkan {jumlah} {pilih} ke keranjang, subtotal Rp{subtotal}")
     else:
         print("Barang tidak tersedia, coba lagi!")
 
+total = 0
+for item in keranjang:
+    total += item[2]
+
+diskon = 0
+if total > 50000:
+    diskon = total * 0.1
+    total -= diskon
+
 print("\n=== Struk Belanja ===")
+print ("barang\tJumlah\tSubtotal")
+for item in keranjang:
+    print(f"{item[0]}\t{item[1]}\tRp{item[2]}")
+
+print ("------------------------")
+print(f"Diskon : Rp {diskon}") 
 print(f"Total belanja: Rp{total}")
